50a97a50dbf859970f522d7a71750d248f090ba5
ddwangx <ddwanglife@163.com>
2018-11-08 18:35:52 +0800

    增加开发说明项，保证服务注册eureka高可用，修改配置文件进行部署验证

--- eureka-server/src/main/java/com/learn/springcloud/application/Application.java
+++ eureka-server/src/main/java/com/learn/springcloud/application/Application.java
@@ -1,5 +1,7 @@
 package com.learn.springcloud.application;
 
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -8,6 +10,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		
+		//1,单服务器注册启动方式
+//		new SpringApplicationBuilder(Application.class).web(true).run(args);
+		//2，模拟两台eureka高可用集群注册方式
+		Scanner scan = new Scanner(System.in);
+		String profiles = scan.nextLine();
+		new SpringApplicationBuilder(Application.class).profiles(profiles).run(args);
 	}
 }
