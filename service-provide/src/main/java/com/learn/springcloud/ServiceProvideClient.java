ffb799e28cff8ca9ace0fd69bd5bb9ffdff068f4
ddwangx <ddwanglife@163.com>
2018-11-09 10:24:11 +0800

    服务消费者初始化

--- service-provide/src/main/java/com/learn/springcloud/ServiceProvideClient.java
+++ service-provide/src/main/java/com/learn/springcloud/ServiceProvideClient.java
@@ -1,12 +1,20 @@
 package com.learn.springcloud;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Scanner;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ServiceProvideClient {
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceProvideClient.class, args);
+		//1,单服务提供者启动方式
+//		SpringApplication.run(ServiceProvideClient.class, args);
+		//2,模拟多服务启动
+		Scanner scan = new Scanner(System.in);
+		String profiles = scan.nextLine();
+		new SpringApplicationBuilder(ServiceProvideClient.class).profiles(profiles).run(args);
 	}
 }
