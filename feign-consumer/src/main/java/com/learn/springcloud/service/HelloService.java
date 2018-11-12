f56440985d11122bddaac0cfdc74216eff9da543
ddwangx <ddwanglife@163.com>
2018-11-12 14:49:24 +0800

    信息初始化，重新变更

--- feign-consumer/src/main/java/com/learn/springcloud/service/HelloService.java
+++ feign-consumer/src/main/java/com/learn/springcloud/service/HelloService.java
@@ -3,7 +3,7 @@ package com.learn.springcloud.service;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("hello-service")
+@FeignClient(value = "hello-service",fallback = HelloHystrixService.class)
 public interface HelloService {
 	@RequestMapping("/hello")
 	String hello();
