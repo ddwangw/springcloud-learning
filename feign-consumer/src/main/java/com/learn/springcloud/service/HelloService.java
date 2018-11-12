bee4f62b9e4692ff17374d561dce4a1ad336b1f2
ddwangx <ddwanglife@163.com>
2018-11-12 09:47:50 +0800

    feign声明式服务调用项目搭建

---
+++ feign-consumer/src/main/java/com/learn/springcloud/service/HelloService.java
@@ -0,0 +1,10 @@
+package com.learn.springcloud.service;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("hello-service")
+public interface HelloService {
+	@RequestMapping("/hello")
+	String hello();
+}
