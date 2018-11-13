c324071fdf95afc63b68e6e22509ef8bb6a9ab92
ddwangx <ddwanglife@163.com>
2018-11-13 08:57:25 +0800

    同步内容到本地库

--- config-server/src/main/java/com/learn/springcloud/ConfigServerApplication.java
+++ config-server/src/main/java/com/learn/springcloud/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.learn.springcloud;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.config.server.EnableConfigServer;
@@ -9,7 +8,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @SpringBootApplication
 public class ConfigServerApplication {
 	public static void main(String[] args) {
-//		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
-		SpringApplication.run(ConfigServerApplication.class, args);
+		new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
 	}
 }
